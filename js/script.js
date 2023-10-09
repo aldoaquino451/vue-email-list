f23a83b1fac05a000d7d1536ec4967adc7993e1d
aldoaquino451 <138690750+aldoaquino451@users.noreply.github.com>
2023-10-09 09:46:21 +0200

    [add] funzione getapi e vif con loading

--- js/script.js
+++ js/script.js
@@ -7,19 +7,26 @@ createApp({
     return {
       booleanAPI: 'https://flynn.boolean.careers/exercises/api/random/mail', // endpoint
       mails: [],
-      max: 10,
+      limit: 200,
     }
   },
   
   methods: {
-    generateMail() {
-      for (let i = 0; i < this.max; i++) {
-        axios.get(this.booleanAPI)
-        .then( (risultato) => {
-          this.mails.push(risultato.data.response)
-        })
-      }
+
+    getAPI() {
+      axios.get(this.booleanAPI)
+      .then( (risultato) => {
+        console.log(risultato.data.response);
+        this.mails.push(risultato.data.response)
+      })
     },
+
+    generateMail() {
+      for (let i = 0; i < this.limit; i++) {
+        this.getAPI();
+      }
+    }
+
   },
 
   mounted() {
